9ffbd4e7f23e651b1fd178e7c3c0aa54ee937e2f
m.kalenishin <m.kalenishin@ylab.io>
2022-08-09 23:00:27 +0300

    feat: added finish remarks

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -75,7 +75,6 @@ namespace CalculatorProgram
 
                 Console.WriteLine("\n"); // Friendly linespacing.
             }
-            // Add call to close the JSON writer before return
             calculator.Finish();
             return;
         }
